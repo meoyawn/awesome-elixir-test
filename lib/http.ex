d2a1cffc420dbde5ff9fb357a772b7b7b85e5286
adelnizamutdinov <stiggpwnz@gmail.com>
2019-08-25 00:29:56 +0300

    user agent

--- lib/http.ex
+++ lib/http.ex
@@ -1,9 +1,11 @@
 defmodule Http do
   @spec get(String.t(), [{charlist, charlist}]) :: charlist | :not_found
   def get(url, headers \\ []) do
-    case :httpc.request(:get, {to_charlist(url), headers}, [], []) do
-      {:ok, {{_ver, 200, 'OK'}, _headers, body}} -> body
-      {:ok, {{_ver, 404, 'OK'}, _headers, _body}} -> :not_found
+    plus_ua = [{'User-Agent', 'httpc'} | headers]
+
+    case :httpc.request(:get, {to_charlist(url), plus_ua}, [], []) do
+      {:ok, {{_ver, 200, _msg}, _headers, body}} -> body
+      {:ok, {{_ver, 404, _msg}, _headers, _body}} -> :not_found
     end
   end
 end
